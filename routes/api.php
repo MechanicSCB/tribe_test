9923252e82ee47d04a0250839155a780a190508e
Nikolay <NikolayUlyanov@yandex.ru>
2024-01-12 10:18:49 +0300

    Add results top and store routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResultController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/top', [ResultController::class, 'index'])->name('results.top');
+Route::post('/results', [ResultController::class, 'store'])->name('results.store');
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
