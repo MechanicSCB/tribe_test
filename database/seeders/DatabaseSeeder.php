1cb59af2a9da37bf487cf7ac1ad15eec9e2cbc6e
Nikolay <NikolayUlyanov@yandex.ru>
2024-01-12 10:23:15 +0300

    Add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Member;
+use App\Models\Result;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Factory::create();
+        $members = [];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        for($i=1;$i<=500;$i++){
+            $members[] = [
+                'id' => $i,
+                'email' => $faker->unique()->email
+            ];
+        }
+
+        Member::query()->upsert($members, ['id']);
+
+        $results = [];
+
+        for($i=1;$i<=10000;$i++){
+            $results[] = [
+                'id' => $i,
+                // пусть результаты будут только у игроков с четным id, чтобы были игроки без результатов
+                'member_id' => rand(0,5) ? rand(1,250)*2 : null,
+                'milliseconds' => rand(1000, 50000),
+            ];
+        }
+
+        Result::query()->upsert($results, ['id']);
     }
 }
